ba88a90db40179132bdb6a3c7abee46da06212bc
Munkhtegsh <munkhtegsh@outlook.com>
2018-12-30 00:24:28 -0800

    Setup dev environment

--- server/server.js
+++ server/server.js
@@ -1,8 +1,14 @@
+require('dotenv').config();
 const express = require('express');
 const bodyParser = require('body-parser');
+const massive = require('massive');
 
 const app = express();
 app.use(bodyParser.json());
+
+massive(process.env.DB_URI).then(instance => {
+  app.set('db', instance)
+})
 
 const PORT = 3001;
 app.listen(PORT, () => console.log(`Port is running on ${PORT}`))
