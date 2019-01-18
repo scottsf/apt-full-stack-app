4b41d2387690c805b394f272053b2ea2e07b2953
Munkhtegsh <munkhtegsh@outlook.com>
2019-01-18 15:58:52 -0800

    Style Dashboard component

--- src/components/wizard/Wizard.js
+++ src/components/wizard/Wizard.js
@@ -1,21 +1,23 @@
 import React, {Component} from 'react';
 import {Route, Link} from 'react-router-dom';
-import axios from 'axios';
 import Step1 from './Step1.js';
 import Step2 from './Step2.js';
 import Step3 from './Step3.js';
 import {connect} from 'react-redux';
 import {clearState} from '../../ducks/reducer';
+import './wizard.scss';
 
 class Wizard extends Component {
   render() {
     return (
-      <div>
-        Wizard
-        <Link to="/">
-          <button onClick={() => this.props.clearState()}> Cancel </button>
-        </Link>
-        <div>
+      <div className="wizard">
+        <div className="wizard_nav">
+          <h2> Add New List </h2>
+          <Link to="/">
+            <button onClick={() => this.props.clearState()}> Cancel </button>
+          </Link>
+        </div>
+        <div className="wizard_steps">
           <Route path="/wizard/step1" component={Step1} />
           <Route path="/wizard/step2" component={Step2} />
           <Route path="/wizard/step3" component={Step3} />
