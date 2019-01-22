cfdfeb848d992e4c4189f4d1a09f1d1450c9f0ff
Munkhtegsh <munkhtegsh.m@gmail.com>
2019-01-21 21:57:45 -0800

    Move the delete btn to the top

--- src/components/house/House.js
+++ src/components/house/House.js
@@ -12,7 +12,7 @@ const House = ({...props}) => (
       <p> Address: {props.address} </p>
       <p> City: {props.city} </p>
       <p> State: {props.state} </p>
-      <button onClick={ _ => props.removeHouse(props.id)}> Delete </button>
+      <button className="houses_btn-delete"onClick={ _ => props.removeHouse(props.id)}> X </button>
     </div>
     <div className="houses_payment">
       <p> Monthly Mortgage: {props.mortgage}</p>
