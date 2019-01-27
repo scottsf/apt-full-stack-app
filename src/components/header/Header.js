b49ff99864855914444287ecda29d6bb6e701dad
Munkhtegsh <munkhtegsh@outlook.com>
2019-01-27 00:51:30 -0800

    Complete authorizing user

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -2,21 +2,27 @@ import React from 'react';
 import {Link} from 'react-router-dom';
 import './header.scss';
 import {connect} from 'react-redux';
+import {updateUser} from '../../ducks/reducer.js'
 
 class Header extends React.Component {
+
+  logOut = () => {
+    this.props.updateUser({user: ''});
+  }
+
   render() {
     return (
       <div className="header">
-        <Link to='/'>
+        <Link to='/' className="header_link">
           <h2 className="header_h2">Houser</h2>
-          {
-            this.props.user
-            ?
-            <h4 className="header_h4"> Logout </h4>
-            :
-            <h4 className="header_h4"> Login </h4>
-          }
         </Link>
+          {
+            !!this.props.user
+            ?
+            <h4 className="header_h4" onClick={() => this.logOut()}> <a href="/api/login">Logout</a> </h4>
+            :
+            <h4 className="header_h4"> <a href="/api/login">Login</a> </h4>
+          }
       </div>
     )
   }
@@ -27,4 +33,4 @@ const mapStateToProps = (state) => (
 )
 
 
-export default connect(mapStateToProps)(Header);
+export default connect(mapStateToProps, {updateUser})(Header);
