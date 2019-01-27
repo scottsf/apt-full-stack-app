90495aec77a910db48381e2df54c35865620e8e1
Munkhtegsh <munkhtegsh@outlook.com>
2019-01-27 00:00:12 -0800

    Style nav

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,13 +1,30 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
 import './header.scss';
+import {connect} from 'react-redux';
 
-const Header = () => (
-  <div className="header">
-    <Link to='/'>
-      <h2 className="header_h2">Houser</h2>
-    </Link>
-  </div>
-);
+class Header extends React.Component {
+  render() {
+    return (
+      <div className="header">
+        <Link to='/'>
+          <h2 className="header_h2">Houser</h2>
+          {
+            this.props.user
+            ?
+            <h4 className="header_h4"> Logout </h4>
+            :
+            <h4 className="header_h4"> Login </h4>
+          }
+        </Link>
+      </div>
+    )
+  }
+}
 
-export default Header;
+const mapStateToProps = (state) => (
+  {user: state.user}
+)
+
+
+export default connect(mapStateToProps)(Header);
